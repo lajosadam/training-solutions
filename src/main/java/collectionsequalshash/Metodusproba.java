d32d3eccb13c68aef026abc4dc3396e6229b28a3
lajos.adam <napimester@gmail.com>
2021-02-03 13:40:52 +0100

    collection

--- src/main/java/collectionsequalshash/Metodusproba.java
+++ src/main/java/collectionsequalshash/Metodusproba.java
@@ -5,10 +5,11 @@ import book.Book;
 import java.util.List;
 import java.util.Set;
 
+/*
 public class Metodusproba {
     public Book findBookByTitleAuthor(List<Book> books, String title, String author);
     public Book findBook(List<Book> books, Book book);
     public Set<Book> addBooksToSet(Book[] books);
 
-    //megint nem értem mi a feladat
-}
+    megint nem értem mi a feladat
+}*/
