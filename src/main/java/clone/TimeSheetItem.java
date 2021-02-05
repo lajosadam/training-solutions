41c0042644ec3ec39302fb7792f494067846b24a
lajos.adam <napimester@gmail.com>
2021-02-05 12:06:52 +0100

    shuffle, reverse, unmodifiableList

---
+++ src/main/java/clone/TimeSheetItem.java
@@ -0,0 +1,63 @@
+package clone;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class TimeSheetItem {
+
+    private String employee;
+
+    private String project;
+
+    private LocalDateTime from;
+
+    private LocalDateTime to;
+
+    public TimeSheetItem(String employee, String project, LocalDateTime from, LocalDateTime to) {
+        this.employee = employee;
+        this.project = project;
+        this.from = from;
+        this.to = to;
+    }
+
+    public TimeSheetItem(TimeSheetItem clone){
+        this.employee = clone.employee;
+        this.project = clone.project;
+        this.from = clone.from;
+        this.to = clone.to;
+    }
+
+    public String getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(String employee) {
+        this.employee = employee;
+    }
+
+    public String getProject() {
+        return project;
+    }
+
+    public void setProject(String project) {
+        this.project = project;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(LocalDateTime from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(LocalDateTime to) {
+        this.to = to;
+    }
+
+    //hibás a feladat leírása!!
+}
