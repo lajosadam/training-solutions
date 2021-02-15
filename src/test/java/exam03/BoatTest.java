2b8cfdff26b1e213407c834a4b4b60f11d2ced5b
lajos.adam <napimester@gmail.com>
2021-02-15 17:16:41 +0100

    exam03 - Tests

---
+++ src/test/java/exam03/BoatTest.java
@@ -0,0 +1,16 @@
+package exam03;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BoatTest {
+
+    @Test
+    void create() {
+        Boat boat = new Boat("WetDreams", 15);
+        assertEquals("WetDreams", boat.getName());
+        assertEquals(15, boat.getMaxPassengers());
+    }
+}
